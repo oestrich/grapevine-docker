e37c1b37f4861c16ee48405b4839c05f228bf167
Eric Oestrich <eric@oestrich.org>
2020-01-28 17:03:57 -0500

    Append package version to docker tags
    Similar to arch, so if the version doesn't change but I build a new
    image, the old image is static.

--- elixir/1.10/build.sh
+++ elixir/1.10/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
 
-docker build -t grapevinehaus/elixir:1.10.0 .
+docker build -t grapevinehaus/elixir:1.10.0-1 .
--- elixir/1.9/build.sh
+++ elixir/1.9/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
 
-docker build -t grapevinehaus/elixir:1.9.4 .
+docker build -t grapevinehaus/elixir:1.9.4-1 .
